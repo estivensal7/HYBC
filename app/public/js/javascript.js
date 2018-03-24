46ca6e9bd7ef3daff5a71eab2ed7b6ee482d3756
unknown <estivensal7@gmail.com>
2018-03-24 00:29:13 -0400

    finished jquery setup to console log new contact-us message

--- app/public/js/javascript.js
+++ app/public/js/javascript.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 	console.log('ready!');
-})
+
+
+	submitContactMessage = (error) => {
+		
+		let submitButton = $("#contact-submit");
+
+		(submitButton).on('click', function() {
+		let firstName = $("#contact-first-name").val();
+		let lastName = $("#contact-last-name").val();
+		let email = $("#contact-email").val();
+		let message = $("#contact-message").val();
+		let	submittedMessage = {
+				'first-name': firstName,
+				'last-name': lastName,
+				'email': email,
+				'message': message
+			}
+
+			if (error) {
+				throw error;
+			} else {
+				console.log(submittedMessage);
+			}
+		});
+	}
+	submitContactMessage();
+});
