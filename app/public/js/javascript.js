d296180620b4c72e5c6c368bfc978ff1161d55d0
unknown <estivensal7@gmail.com>
2018-03-24 02:20:20 -0400

    finished most of jquery for checkout page, returning customer information

--- app/public/js/javascript.js
+++ app/public/js/javascript.js
@@ -26,4 +26,49 @@ $(document).ready(function() {
 		});
 	}
 	submitContactMessage();
+
+	submitCustomerInformation = (error) => {
+
+		let submitButton = $('#checkout-submit-btn');
+
+		(submitButton).on('click', function() {
+			let firstName = $("#checkout-first-name").val();
+			let lastName = $("#checkout-last-name").val();
+			let email = $("#checkout-email").val();
+			let address = $("#checkout-address").val();
+			let addressTwo = $("#checkout-address-two").val();
+			let city = $("#checkout-city").val();
+			let state = $("#checkout-state").val();
+			let zipCode = $("#checkout-zip-code").val();
+			let cardName = $("#name-on-card").val();
+			let cardNumber = $("#card-number").val();
+			let expirationMonth = $("#expiry-month").val();
+			let expirationYear = $("#expiry-year").val();
+			let cardCvv = $("#cvv-code").val();
+			let cardType = $('input[name=gridRadios]:checked').val();
+
+			let customerInformation = {
+				'first name': firstName,
+				'last name': lastName,
+				'email': email,
+				'address': address,
+				'address-two': addressTwo,
+				'city': city,
+				'state': state,
+				'zip-code': zipCode,
+				'name on card': cardName,
+				'card number': cardNumber,
+				'expiration month': expirationMonth,
+				'expiration year': expirationYear,
+				'CVV code': cardCvv,
+				'card type': cardType
+			}
+
+			console.log(customerInformation);
+			alert('Thank you for your order!');
+		})
+	}
+
+	submitCustomerInformation();
 });
+
